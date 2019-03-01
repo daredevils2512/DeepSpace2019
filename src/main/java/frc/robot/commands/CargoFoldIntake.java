1f90da8aa733215d25f3c86023887f11f70b0e7e
Jared <digibyteme@gmail.com>
2019-02-28 19:55:24 -0600

    Added distance check to folding the ball intake

--- src/main/java/frc/robot/commands/CargoFoldIntake.java
+++ src/main/java/frc/robot/commands/CargoFoldIntake.java
@@ -10,6 +10,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import frc.robot.Robot;
+import frc.robot.RobotMap;
 
 public class CargoFoldIntake extends Command {
 
@@ -30,7 +31,11 @@ public class CargoFoldIntake extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.m_cargoIntake.foldIntake(this.m_foldDir);
+    if (Robot.m_ballDistanceSensor.getDistance() >= 18) {
+      Robot.m_cargoIntake.foldIntake(this.m_foldDir);
+    } else {
+      Robot.m_cargoIntake.foldIntake(RobotMap.cargoUpPos);
+    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
