2464a742b0a54ae374b0184e2423b49e5cbffec1
Jared <digibyteme@gmail.com>
2019-03-07 09:59:51 -0600

    Added drive to wall. untested

---
+++ src/main/java/frc/robot/commands/DriveToWall.java
@@ -0,0 +1,46 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.constants.*;
+import frc.robot.constants.Constants.DistanceSensorSide;
+
+public class DriveToWall extends Command {
+
+    private double m_distance;
+    private double currentDist;
+    private DistanceSensorSide m_sensor;
+
+    public DriveToWall(double distance, DistanceSensorSide sensor) {
+        requires(Robot.m_Drivetrain);
+        m_distance = distance;
+        m_sensor = sensor;
+
+    }
+
+    @Override
+    public void execute() {
+      // 0% power at 0in
+      // 100% power at 24in
+      switch (this.m_sensor) {
+        case BALL:
+          this.currentDist = Robot.m_ballDistanceSensor.getDistance();
+          break;
+        case HATCH:
+          this.currentDist = Robot.m_hatchDistanceSensor.getDistance();
+      }
+      double diff = this.currentDist - this.m_distance;
+      double speed;
+
+        if (diff > 0) {
+          speed = Math.min(1.0, (diff / 24));
+            Robot.m_Drivetrain.arcadeDrive(speed, 0);
+        } else {
+            Robot.m_Drivetrain.arcadeDrive(0, 0);
+        }
+    }
+
+    public boolean isFinished() {
+        return true;
+    }
+}
