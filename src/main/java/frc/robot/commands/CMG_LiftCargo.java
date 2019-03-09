e8556c1abca960d55ba6922e288b43758a87ffbf
ike <ihschw241@isd709.org>
2019-03-09 11:34:26 -0600

    lift cargo done and ready for testing

--- src/main/java/frc/robot/commands/CMG_LiftCargo.java
+++ src/main/java/frc/robot/commands/CMG_LiftCargo.java
@@ -1,12 +1,12 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.constants.Constants.LiftPosition;
 
 public class CMG_LiftCargo extends CommandGroup {
 
     public CMG_LiftCargo() {
         addSequential(new CargoFoldUp());
-        // addSequential(new RunToHeight) 
-        //TODO make run to height command for lift
+        addSequential(new RunToPosition(LiftPosition.HATCHBOTTOM)); 
     }
 }
