c641eeaccd6714772e4a9354817d642e77bdef7a
ike <ihschw241@isd709.org>
2019-07-19 14:36:56 -0500

    save for gitch code

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,19 +7,19 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+// import edu.wpi.first.wpilibj.SpeedControllerGroup;
+// import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.*;
 
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 import frc.robot.commands.ArcadeDrive;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.*;
+// import com.ctre.phoenix.motorcontrol.NeutralMode;
+// import com.ctre.phoenix.motorcontrol.can.*;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
 import com.revrobotics.CANSparkMax;
@@ -28,7 +28,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 // import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 // import edu.wpi.first.wpilibj.CounterBase.EncodingType;
-import edu.wpi.first.wpilibj.*;
+// import edu.wpi.first.wpilibj.*;
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
@@ -37,10 +37,10 @@ public class Drivetrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-  private WPI_TalonSRX rightSpark;
-  private WPI_TalonSRX leftSpark;
-  private WPI_TalonSRX leftRearSpark;
-  private WPI_TalonSRX rightRearSpark;
+  private CANSparkMax rightSpark;
+  private CANSparkMax leftSpark;
+  private CANSparkMax leftRearSpark;
+  private CANSparkMax rightRearSpark;
   private static DifferentialDrive drivetrain;
   private Encoder leftEncoder;
   private Encoder rightEncoder;
@@ -58,28 +58,28 @@ public class Drivetrain extends Subsystem {
 
   public Drivetrain() {
 
-    leftSpark = new WPI_TalonSRX(RobotMap.leftSparkID);    
-    rightSpark = new WPI_TalonSRX(RobotMap.rightSparkID);
-    leftRearSpark = new WPI_TalonSRX(RobotMap.leftRearSparkID);
-    rightRearSpark = new WPI_TalonSRX(RobotMap.rightRearSparkID);
+    leftSpark = new CANSparkMax(RobotMap.leftSparkID, MotorType.kBrushless);    
+    rightSpark = new CANSparkMax(RobotMap.rightSparkID, MotorType.kBrushless);
+    leftRearSpark = new CANSparkMax(RobotMap.leftRearSparkID, MotorType.kBrushless);
+    rightRearSpark = new CANSparkMax(RobotMap.rightRearSparkID, MotorType.kBrushless);
 
     leftRearSpark.follow(leftSpark);
     rightRearSpark.follow(rightSpark);
 
-    // leftSpark.setIdleMode(IdleMode.kCoast);
-    // rightSpark.setIdleMode(IdleMode.kCoast);
-    // leftRearSpark.setIdleMode(IdleMode.kCoast);
-    // rightRearSpark.setIdleMode(IdleMode.kCoast);
+    leftSpark.setIdleMode(IdleMode.kCoast);
+    rightSpark.setIdleMode(IdleMode.kCoast);
+    leftRearSpark.setIdleMode(IdleMode.kCoast);
+    rightRearSpark.setIdleMode(IdleMode.kCoast);
 
-    // leftSpark.setOpenLoopRampRate(0.25);
-    // rightSpark.setOpenLoopRampRate(0.25);
-    // leftRearSpark.setOpenLoopRampRate(0.25);
-    // rightRearSpark.setOpenLoopRampRate(0.25);
+    leftSpark.setOpenLoopRampRate(0.25);
+    rightSpark.setOpenLoopRampRate(0.25);
+    leftRearSpark.setOpenLoopRampRate(0.25);
+    rightRearSpark.setOpenLoopRampRate(0.25);
 
-    // leftSpark.setSmartCurrentLimit(65, 10);
-    // rightSpark.setSmartCurrentLimit(65, 10);
-    // leftRearSpark.setSmartCurrentLimit(65, 10);
-    // rightRearSpark.setSmartCurrentLimit(65, 10);
+    leftSpark.setSmartCurrentLimit(65, 10);
+    rightSpark.setSmartCurrentLimit(65, 10);
+    leftRearSpark.setSmartCurrentLimit(65, 10);
+    rightRearSpark.setSmartCurrentLimit(65, 10);
         
     leftEncoder = new Encoder(RobotMap.leftEncoderChannelA, RobotMap.leftEncoderChannelB, false, CounterBase.EncodingType.k4X);
     rightEncoder = new Encoder(RobotMap.rightEncoderChannelA, RobotMap.rightEncoderChannelB, true, CounterBase.EncodingType.k4X);
